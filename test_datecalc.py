a4bc94d2830d75793ead4c2fe27f221cdc30e319
Beth Lockwood <bethlockwood814@gmail.com>
2022-08-22 15:56:53 +0100

    1 day tests added

--- test_datecalc.py
+++ test_datecalc.py
@@ -1,5 +1,5 @@
 from datecalc import duration, when
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 
 def test_zero_duration():
@@ -14,6 +14,7 @@ def test_zero_duration():
     # Assert
     assert actual_output == expected_output
 
+
 def test_zero_when():
     # Arrange
     start_date = date.today()
@@ -25,3 +26,29 @@ def test_zero_when():
 
     # Assert
     assert actual_output == expected_output
+
+
+    def test_1_day_duration():
+        # Arrange
+        start_date = date.today()
+        end_date = date.today() + timedelta(1)
+        expected_output = 1
+
+        # Act
+        actual_output = duration(start_date, end_date)
+
+        # Assert
+        assert actual_output == expected_output
+
+
+    def test_1_day_when():
+        # Arrange
+        start_date = date.today()
+        days_between = 1
+        expected_output = date.today() + timedelta(1)
+
+        # Act
+        actual_output = when(start_date, days_between)
+
+        # Assert
+        assert actual_output == expected_output
