4c1c4978b47fa4f863033c70003bb11b07249be5
Beth Lockwood <bethlockwood814@gmail.com>
2022-08-25 19:00:47 +0100

    Removed unnecessary import

--- datecalc.py
+++ datecalc.py
@@ -2,11 +2,14 @@
 Calculates difference between dates and adds/subtracts dates
 '''
 
-from datetime import datetime, timedelta
+#import libraries
+from datetime import timedelta
+
 
 # calculates the difference between two dates in days
 def duration(start_date, end_date):
     return abs((end_date - start_date).days)
+
 
 # calculates a new date, by adding an amount of days to a given date
 def when(start_date, days_between):
