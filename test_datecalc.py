8e2c504ff043620d18d2b1b43642f791350d0b31
Beth Lockwood <bethlockwood814@gmail.com>
2022-08-25 13:10:59 +0100

    Fixed indentation so all 4 tests run

--- test_datecalc.py
+++ test_datecalc.py
@@ -28,27 +28,27 @@ def test_zero_when():
     assert actual_output == expected_output
 
 
-    def test_1_day_duration():
-        # Arrange
-        start_date = date.today()
-        end_date = date.today() + timedelta(1)
-        expected_output = 1
+def test_1_day_duration():
+    # Arrange
+    start_date = date.today()
+    end_date = date.today() + timedelta(1)
+    expected_output = 1
 
-        # Act
-        actual_output = duration(start_date, end_date)
+    # Act
+    actual_output = duration(start_date, end_date)
 
-        # Assert
-        assert actual_output == expected_output
+    # Assert
+    assert actual_output == expected_output
 
 
-    def test_1_day_when():
-        # Arrange
-        start_date = date.today()
-        days_between = 1
-        expected_output = date.today() + timedelta(1)
+def test_1_day_when():
+    # Arrange
+    start_date = date.today()
+    days_between = 1
+    expected_output = date.today() + timedelta(1)
 
-        # Act
-        actual_output = when(start_date, days_between)
+    # Act
+    actual_output = when(start_date, days_between)
 
-        # Assert
-        assert actual_output == expected_output
+    # Assert
+    assert actual_output == expected_output
