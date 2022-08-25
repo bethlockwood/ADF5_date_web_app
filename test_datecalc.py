94eece1bf937d3e6cc542669ca2894288b8eadf0
Beth Lockwood <bethlockwood814@gmail.com>
2022-08-25 18:45:00 +0100

    added tests for 999999 days

--- test_datecalc.py
+++ test_datecalc.py
@@ -1,7 +1,9 @@
+# import libraries and functions
 from datecalc import duration, when
 from datetime import date, datetime, timedelta
 
 
+# test - the same date is input for start and end date
 def test_zero_duration():
     # Arrange
     start_date = date.today()
@@ -15,6 +17,7 @@ def test_zero_duration():
     assert actual_output == expected_output
 
 
+# test - an input duration of 0 days
 def test_zero_when():
     # Arrange
     start_date = date.today()
@@ -28,6 +31,7 @@ def test_zero_when():
     assert actual_output == expected_output
 
 
+# test - the date input for start and end date have a difference of 1 day
 def test_1_day_duration():
     # Arrange
     start_date = date.today()
@@ -41,11 +45,40 @@ def test_1_day_duration():
     assert actual_output == expected_output
 
 
+# test - an input duration of 1 day
 def test_1_day_when():
     # Arrange
     start_date = date.today()
     days_between = 1
     expected_output = date.today() + timedelta(1)
+
+    # Act
+    actual_output = when(start_date, days_between)
+
+    # Assert
+    assert actual_output == expected_output
+
+
+# test - the date input for start and end date have a difference of 999999 days
+def test_999999_day_duration():
+    # Arrange
+    start_date = date.today()
+    end_date = date.today() + timedelta(999999)
+    expected_output = 999999
+
+    # Act
+    actual_output = duration(start_date, end_date)
+
+    # Assert
+    assert actual_output == expected_output
+
+
+# test - an input duration of 999999 day
+def test_999999_day_when():
+    # Arrange
+    start_date = date.today()
+    days_between = 999999
+    expected_output = date.today() + timedelta(999999)
 
     # Act
     actual_output = when(start_date, days_between)
